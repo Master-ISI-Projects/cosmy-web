9bc8398b21ae02c04227fb0d7eb882cfc01cc329
Mouad ZIANI <mouad.ziani1997@gmail.com>
2020-02-08 22:14:13 +0100

    [website] integrate html template and add base components

--- web/projects/website/src/app/app.module.ts
+++ web/projects/website/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './router/app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './views/components/navbar/navbar.component';
+import { FooterComponent } from './views/components/footer/footer.component';
+import { TopNavbarComponent } from './views/components/top-navbar/top-navbar.component';
+import { BlankComponent } from './views/pages/blank/blank.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavbarComponent,
+    NavbarComponent,
+    FooterComponent,
+    BlankComponent
   ],
   imports: [
     BrowserModule,
