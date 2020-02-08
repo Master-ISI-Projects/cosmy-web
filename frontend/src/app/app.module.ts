2fc3f012d682f1549070ea87a91444c159d4bdf6
Mouad ZIANI <mouad.ziani1997@gmail.com>
2020-02-08 16:58:44 +0100

    [frontend] convert template to angular components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ConversationComponent } from './conversation/conversation.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ConversationComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
--- frontend/src/app/modules/chatter/chatter.module.ts
+++ frontend/src/app/modules/chatter/chatter.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ConversationComponent } from './conversation/conversation.component';
 
-
-
 @NgModule({
   declarations: [ConversationComponent],
   imports: [
     CommonModule
+  ],
+  exports: [
+    ConversationComponent
   ]
 })
 export class ChatterModule { }
