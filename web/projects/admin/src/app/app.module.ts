5d196a729ef6d86a34bc7ea94895dfb2d5d3a470
Mouad ZIANI <mouad.ziani1997@gmail.com>
2020-02-09 20:45:37 +0100

    [backend] convert admin template to angular components

--- web/projects/admin/src/app/app.module.ts
+++ web/projects/admin/src/app/app.module.ts
@@ -1,12 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './router/app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './views/components/navbar/navbar.component';
+import { SidebarComponent } from './views/components/sidebar/sidebar.component';
+import { FooterComponent } from './views/components/footer/footer.component';
+import { MainComponent } from './views/layouts/main/main.component';
+import { BlankComponent } from './views/pages/blank/blank.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    FooterComponent,
+    MainComponent,
+    BlankComponent
   ],
   imports: [
     BrowserModule,
--- web/projects/admin/src/app/router/app-routing.module.ts
+++ web/projects/admin/src/app/router/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BlankComponent } from '../views/pages/blank/blank.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: BlankComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
